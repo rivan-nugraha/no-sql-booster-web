1ba5e4e41235b8c2a7938b4b86e402c6c7378870
Irvan Nugraha <piksi.irvan.18402043@gmail.com>
2026-03-31 14:25:27 +0700

    Clearing V1

--- src/client/database.test.ts
+++ src/client/database.test.ts
@@ -2,7 +2,7 @@ import { httpRequest } from './http';
 import type { ApiResponse } from './model/response';
 
 export async function testDatabaseConnection(uri: string) {
-  return httpRequest<ApiResponse<{ ok: boolean }>>('/v1/databases/test', {
+  return httpRequest<ApiResponse<{ ok: boolean }>>('/databases/test', {
     method: 'POST',
     body: { uri },
   });
